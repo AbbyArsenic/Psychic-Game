8221a518a60dd8776274031fce4392af6bdca708
AbbyArsenic <brit13@gmail.com>
2017-08-05 23:59:27 -0700

    added bootstrap styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,66 +12,61 @@ var letters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M",
             "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var psychicLetter;
 
-var random = function() {
+function random() {
     psychicLetter = letters[Math.floor(Math.random() * letters.length)];
-};
+}
 random();
-console.log(psychicLetter);
 
-    //gather user input from keypress
-    document.onkeyup = function(e) {
-        //check that there are guesses remaining and entry is a letter
-        if (e.which === 13) {
-            //reset win/loss count/display
-            wins = 0;
-            losses = 0;
+//gather user input from keypress
+document.onkeyup = function(e) {
+    //PRESS ENTER TO RESET GAME
+    if (e.which === 13) {
+        //reset win/loss count/display
+        wins = 0;
+        losses = 0;
+        showWin.innerHTML = wins;
+        showLoss.innerHTML = losses;
+        //reset guesses
+        remaining = 9;
+        showLeft.innerHTML = remaining;
+        guesses = [];
+        showGuess.innerHTML = "";
+        //call new psychicLetter
+        random();
+    //check that there are guesses remaining and entry is a letter
+    } else if (remaining > 0 && e.which >= 65 && e.which <= 90) {
+        //assign key to userGuess
+        var userGuess = e.key;
+        //check for win condition
+        if (userGuess.toUpperCase() === psychicLetter) {
+            //add to win count
+            wins++;
+            alert("You must be psychic! The letter was " + psychicLetter + ".");
             showWin.innerHTML = wins;
-            showLoss.innerHTML = losses;
-            //reset guesses
-            remaining = 9;
-            showLeft.innerHTML = remaining;
-            guesses = [];
-            //call new psychicLetter
-            random();
-            console.log(psychicLetter);
-        } else if (remaining > 0 && e.which >= 65 && e.which <= 90) {
-            console.log(remaining);
-            //assign key to userGuess
-            var userGuess = e.key;
-            //check for win condition
-            if (userGuess.toUpperCase() === psychicLetter) {
-                //add to win count
-                wins++;
-                alert("You must be psychic! The letter was " + psychicLetter + ".");
-                showWin.innerHTML = wins;
-                //reset guesses
-                remaining = 9;
-                guesses = [];
-                showGuess.innerHTML = "";
-                //call new psychicLetter
-                random();
-                console.log(psychicLetter);
-            } else {
-                //add each guess to the guesses array
-                guesses.push(" " + userGuess);
-                showGuess.innerHTML = guesses;
-                console.log(guesses);
-                //decrement remaining guesses
-                remaining--;
-                showLeft.innerHTML = remaining;
-            }
-        } else if (remaining === 0) {
-            //add to losses count
-            losses++;
-            alert("Out of guesses; you lose this round.");
-            showLoss.innerHTML = losses;
             //reset guesses
             remaining = 9;
             guesses = [];
             showGuess.innerHTML = "";
             //call new psychicLetter
             random();
-            console.log(psychicLetter);
+        } else {
+            //add each guess to the guesses array
+            guesses.push(" " + userGuess);
+            showGuess.innerHTML = guesses;
+            //decrement remaining guesses
+            remaining--;
+            showLeft.innerHTML = remaining;
         }
-    };
-//}
+    } else if (remaining === 0) {
+        //add to losses count
+        losses++;
+        alert("Out of guesses; you lose this round.");
+        showLoss.innerHTML = losses;
+        //reset guesses
+        remaining = 9;
+        guesses = [];
+        showGuess.innerHTML = "";
+        //call new psychicLetter
+        random();
+    }
+};
